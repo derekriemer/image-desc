8d58f2689c047bbe682c7ed4036ec774223fbe99
derek riemer <git@derekriemer.com>
2025-03-25 00:34:39 -0600

    Add a property on description to get a file name sanatized string for the given short description. it doesn't yet have the extension.

--- api/descriptions.py
+++ api/descriptions.py
@@ -1,11 +1,17 @@
+import re
 from typing import List
+
 from pydantic import BaseModel
 
 
 class Description(BaseModel):
-    short: str
+    title: str
     people: List[str]
     long_description: str
+
+    @property
+    def fileName(self):
+        return re.sub(r"[^a-zA-Z0-9._- ]", " ", self.title)
 
 
 class Entities(BaseModel):
