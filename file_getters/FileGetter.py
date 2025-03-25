59b00d6500b1668178cd02c0a5ba83575b19abc3
derek riemer <git@derekriemer.com>
2025-03-25 00:36:33 -0600

    Remove unused file utils. The api routes will be in charge of instanciating the correct file system.

--- file_getters/FileGetter.py
+++
@@ -1,6 +0,0 @@
-from fsspec.spec import AbstractFileSystem
-from abc import ABC, abstractmethod
-
-class FileGetter(ABC):
-    @abstractmethod
-    def get_file(self)-> File:
--- file_getters/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-A utility that gets files from a source.
-By default, the program gets files from the local file system, but Google Drive, or other file sources may eventually be supported.
-"""
