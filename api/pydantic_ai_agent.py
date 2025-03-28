230a9320f7ee4651bd4f9c0989522c9b728f167f
derek riemer <git@derekriemer.com>
2025-03-28 09:10:04 -0600

    add a class method to get the agent for pydantic_ai. This allows us to override the model dynamically based on whether we are disabling model requests. The classmethod is also just more pythonic and better OOD.

--- api/pydantic_ai_agent.py
+++ api/pydantic_ai_agent.py
@@ -1,9 +1,11 @@
 import logging
+from io import BytesIO
 from typing import IO
 
-
+import fsspec
 from PIL import Image
 from pydantic_ai import Agent, BinaryContent, RunContext
+from pydantic_ai import models
 
 from .context import Context
 from .descriptions import Description
@@ -11,19 +13,6 @@ from .exceptions import AiProcessingException
 from .system_prompt import description_system_prompt
 
 logger = logging.getLogger(__name__)
-
-
-def get_agent() -> Agent:
-    agent = Agent(
-        model="gpt-4o-mini",
-        deps_type=Context,
-    )
-
-    @agent.system_prompt
-    def system_prompt(ctx: RunContext[Context]) -> str:
-        return description_system_prompt + "\n" + ctx.deps.model_dump_json()
-
-    return agent
 
 
 class ImageDescriber:
@@ -34,14 +23,27 @@ class ImageDescriber:
     Each agent can b run against arbitrary agents, but for now, context cannot be updated.
     """
 
+    @classmethod
+    def get_agent(cls) -> Agent:
+        agent = Agent(
+            model=("gpt-4o-mini" if models.ALLOW_MODEL_REQUESTS else "test"),
+            deps_type=Context,
+        )
+
+        @agent.system_prompt
+        def system_prompt(ctx: RunContext[Context]) -> str:
+            return description_system_prompt + "\n" + ctx.deps.model_dump_json()
+
+        return agent
+
     def __init__(self, deps: Context):
         self.deps = deps
-        self.agent = get_agent()
+        self.agent = ImageDescriber.get_agent()
 
-    def _getMimeType(self, image_file: IO) -> str:
+    def _get_mime_type(self, image_data: bytes) -> str:
         """ Gets a mimetype from a file using pillow. """
         try:
-            image = Image.open(image_file)
+            image = Image.open(BytesIO(image_data))
             mime_type = image.get_format_mimetype()
             if mime_type:
                 return mime_type
@@ -50,12 +52,15 @@ class ImageDescriber:
                 "Error processing mimetype for image file", exc_info=e)
             raise AiProcessingException() from e
 
-    async def describe_image(self, image_file) -> Description:
+    async def describe_image(self, image_path: str) -> Description:
+        with open(image_path, 'rb') as f:
+            image_bytes = f.read()
+
         response = await self.agent.run(
-            BinaryContent(
-                image_file, media_type=self._getMimeType(image_file)),
+            [BinaryContent(
+                image_bytes, media_type=self._get_mime_type(image_bytes))],
             result_type=Description,
-        )
+            deps=self.deps)
         description = response.data
         if not description:
             raise AiProcessingException("No description generated.")
